736633c8f08586f793deda66c5a339e94bd148f1
kimyezzang97 <kimyezzang97@gmail.com>
2024-03-19 19:00:16 +0900

    Helper Classes - 다양한 헬퍼 클래스

---
+++ src/pages/ClassesVariables.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page class="q-pa-xl">
+    <section class="q-mb-xl">
+      <div class="text-h4">Shadow</div>
+      <q-separator class="q-my-md" />
+      <div class="q-gutter-y-sm">
+        <div class="shadow-1 q-pa-sm">shadow-1</div>
+        <div class="shadow-7 q-pa-sm">shadow-7</div>
+        <div class="shadow-24 q-pa-sm">shadow-24</div>
+      </div>
+    </section>
+    <section class="q-mb-xl">
+      <div class="text-h4">Visibility</div>
+      <q-separator class="q-my-md" />
+      <div class="ellipsis">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero
+        laborum similique eveniet ullam beatae fugiat accusamus inventore saepe
+        officiis rem, laudantium fugit enim consectetur quidem! Voluptatem,
+        vitae! Necessitatibus, aperiam.
+      </div>
+      <hr />
+      <div class="ellipsis-2-lines">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero
+        laborum similique eveniet ullam beatae fugiat accusamus inventore saepe
+        officiis rem, laudantium fugit enim consectetur quidem! Voluptatem,
+        vitae! Necessitatibus, aperiam.
+      </div>
+      <hr />
+      <div class="ellipsis-3-lines">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero
+        laborum similique eveniet ullam beatae fugiat accusamus inventore saepe
+        officiis rem, laudantium fugit enim consectetur quidem! Voluptatem,
+        vitae! Necessitatibus, aperiam.
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
