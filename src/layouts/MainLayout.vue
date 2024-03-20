6fd6a8ce5666950bf979f39f5ac0add155bc1a98
kimyezzang97 <kimyezzang97@gmail.com>
2024-03-20 17:41:55 +0900

    Quasar Utils

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -114,6 +114,12 @@ const linksList = [
     icon: 'school',
     to: '/form-handling',
   },
+  {
+    title: 'Quasar Utils',
+    caption: 'quasar.dev',
+    icon: 'school',
+    to: '/quasar-utils',
+  },
 ];
 </script>
 
