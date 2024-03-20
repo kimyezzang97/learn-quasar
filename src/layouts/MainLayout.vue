54a65755c3d0a0773a182c36b3e6369da4b0d2e3
kimyezzang97 <kimyezzang97@gmail.com>
2024-03-20 15:08:23 +0900

    Q Form - Form Handling(With Validation)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -108,6 +108,12 @@ const linksList = [
   //   icon: 'school',
   //   to: '/flex-grid-2',
   // },
+  {
+    title: 'Form Handling',
+    caption: 'quasar.dev',
+    icon: 'school',
+    to: '/form-handling',
+  },
 ];
 </script>
 
